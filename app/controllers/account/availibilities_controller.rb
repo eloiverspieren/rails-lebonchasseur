3dd7962ee161c3bcb8f205fa62d51c602229ae51
eloi verspieren <eloi.verspieren@gmail.com>
2016-08-15 19:22:08 +0200

    hop

---
+++ app/controllers/account/availibilities_controller.rb
@@ -0,0 +1,42 @@
+class AvailibilitiesController < ApplicationController
+  def index
+    @availibilities = current_user.availibilities
+  end
+
+  def show
+    @availibility = Availibility.find(params[:id])
+  end
+
+  def new
+   @availibility = Availibility.new
+  end
+
+  def create
+    @availibility = Availibility.new(hunts_params)
+    @availibility.user = current_user
+    @availibility.save
+    redirect_to availibilities_path
+  end
+
+  def edit
+     @availibility = Availibility.find(params[:id])
+  end
+
+  def update
+    @availibility = Availibility.find(params[:id])
+    @availibility.update(availibilities_params)
+    redirect_to availibilities_path(@availibility)
+  end
+
+  def destroy
+    @availibility = Availibility.find(params[:id])
+    @availibility.destroy
+    redirect_to availibilities_path
+  end
+
+  private
+
+  def availibilities_params
+    params.require(:availibility).permit(:start_date, :end_date)
+  end
+end
